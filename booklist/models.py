c954b519f13f1d27eef3860d2cd02adcedd60f1f
Joanna <joanna.gargas@gmail.com>
2017-08-27 15:58:08 +0200

    Add Author and Book model - start version

--- booklist/models.py
+++ booklist/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    firstname = models.CharField(max_length=50)
+    secondname = models.CharField(max_length=50, default="")
+    surname = models.CharField(max_length=50)
+
+
+class Book(models.Model):
+    author = models.OneToOneField(Author)
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200, default="")
+    year_of_publish = models.IntegerField()
