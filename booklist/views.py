789da98adda9eccc4b778fbc76153e8ecab35e76
Joanna <joanna.gargas@gmail.com>
2017-08-28 10:33:43 +0200

    Add mdbootstrap static files (css, js) and improve main view to display book

--- booklist/views.py
+++ booklist/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
 
+from booklist.models import Book
+
 
 def home(request):
-    return render(request, 'home.html')
+    books = Book.objects.all()
+    data = {"books": books}
+    return render(request, 'home.html', context=data)
+
