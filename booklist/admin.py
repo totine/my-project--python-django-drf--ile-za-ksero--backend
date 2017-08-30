2b2d3e61f654e6cdad9e68da69417b49f9650cc6
Joanna <joanna.gargas@gmail.com>
2017-08-30 21:09:37 +0200

    Register book models in admin

--- booklist/admin.py
+++ booklist/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from booklist.models import Author, Book, BookAuthor
+from booklist.models import *
 
-admin.site.register(Author)
+
+admin.site.register(Person)
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Publisher)
+admin.site.register(Edition)
+admin.site.register(Language)
+admin.site.register(ForeignEdition)
+admin.site.register(BookSeries)
 admin.site.register(Book)
-admin.site.register(BookAuthor)
+admin.site.register(XeroBook)
+admin.site.register(Author)
+admin.site.register(Format)
+admin.site.register(NumberOfPages)
+admin.site.register(Part)
+admin.site.register(Cover)
