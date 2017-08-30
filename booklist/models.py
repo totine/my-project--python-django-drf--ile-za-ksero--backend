0a568c4150254f6bcfcd0d5626fe3e2919988ae9
Joanna <joanna.gargas@gmail.com>
2017-08-30 17:49:34 +0200

    Improve Book and related classes in model

--- booklist/models.py
+++ booklist/models.py
@@ -3,27 +3,10 @@ from django.db import models
 # Create your models here.
 
 
-class Author(models.Model):
+class Person(models.Model):
     firstname = models.CharField(max_length=50)
     secondname = models.CharField(max_length=50, default="")
     surname = models.CharField(max_length=50)
-
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    subtitle = models.CharField(max_length=200, default="")
-    isbn = models.IntegerField()
-
-
-class BookAuthor(models.Model):
-    book = models.ForeignKey(Book)
-    author = models.ForeignKey(Author)
-    is_editor = models.BooleanField(default=False)
-    position = models.IntegerField()
-
-    class Meta:
-        ordering = ['position']
-        unique_together = ('book', 'author', 'is_editor', 'position')
 
 
 class Country(models.Model):
@@ -46,6 +29,35 @@ class Edition(models.Model):
     comment = models.CharField(max_length=50)
 
 
+class ForeignEdition(models.Model):
+    number = models.PositiveIntegerField(default=1)
+    year = models.PositiveIntegerField()
+    publisher = models.ForeignKey(Publisher)
+
+
 class BookSeries(models.Model):
     name = models.CharField(max_length=100)
     publisher = models.ForeignKey(Publisher)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200, default="")
+    isbn = models.PositiveIntegerField(default=0)
+    edition = models.OneToOneField(Edition)
+    foreign_edition = models.OneToOneField(ForeignEdition, default=None)
+    series = models.ForeignKey(BookSeries, default=None)
+
+
+class Author(models.Model):
+    book = models.ForeignKey(Book)
+    author = models.ForeignKey(Person)
+    is_editor = models.BooleanField(default=False)
+    position = models.IntegerField()
+
+    class Meta:
+        ordering = ['position']
+        unique_together = ('book', 'author', 'is_editor', 'position')
+
+
+
