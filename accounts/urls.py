34958052cd8d62c4e9beb9806912839df8f3daee
Joanna <joanna.gargas@gmail.com>
2017-09-01 14:17:17 +0200

    Make some settings for account system (urls, register app, simple view)

---
+++ accounts/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+from accounts import views
+
+urlpatterns = [
+    url(r'^$', views.login)
+]
--- bookbase/urls.py
+++ bookbase/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('booklist.urls'))
+    url(r'^', include('booklist.urls')),
+    url(r'^account/', include('accounts.urls'))
 ]
