86563302774fc22b9329a7702a09298a71823d33
Joanna <joanna.gargas@gmail.com>
2017-09-09 22:23:17 +0200

    Improve main page view (change Book to XeroBook)

--- booklist/views.py
+++ booklist/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, HttpResponse
 
-from booklist.models import Book
+from booklist.models import XeroBook
 
 
 def home(request):
-    books = Book.objects.all()
+    books = XeroBook.objects.all()
+    print(books[0].book.author_set.all(), books[0].book.number_of_pages_id, books[0].book)
     data = {"books": books}
     return render(request, 'home.html', context=data)
 
