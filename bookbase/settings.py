b2077e7ca28263ad99c457934d5a6eddfd31457d
Joanna <joanna.gargas@gmail.com>
2017-09-12 18:51:24 +0200

    Change static path settings and add  mathfilters app

--- bookbase/settings.py
+++ bookbase/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mathfilters',
 ]
 
 MIDDLEWARE = [
@@ -124,5 +125,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "bookbase/static/"),
+
+]
 
 STATIC_URL = '/static/'
