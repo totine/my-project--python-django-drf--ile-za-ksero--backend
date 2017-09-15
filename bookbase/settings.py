5a201a703597bb5255c51f33fd2e6aafba11aed6
Joanna <joanna.gargas@gmail.com>
2017-09-15 20:25:55 +0200

    Change settings for static files - next try

--- bookbase/settings.py
+++ bookbase/settings.py
@@ -141,5 +141,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
