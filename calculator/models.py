4f6e24aafc6efb51a644109ffcd630ee37060de4
Joanna <joanna.gargas@gmail.com>
2017-09-19 17:57:39 +0200

    Improve calc form to use post method and model functions

--- calculator/models.py
+++ calculator/models.py
@@ -2,12 +2,18 @@ from django.db import models
 
 
 class XeroCalc(models.Model):
+    XERO_COSTS = [6, 7, 8, 9, 10]
+    BIND_COSTS = [1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 6]
     number_of_pages = models.PositiveIntegerField(default=0)
     cost_per_page = models.DecimalField(max_digits=2, decimal_places=2)
     bind_cost = models.DecimalField(max_digits=3, decimal_places=2)
 
     class Meta:
         managed = False
+
+    @property
+    def cost_per_page_in_grosz(self):
+        return int(self.cost_per_page * 100)
 
     def calc_xero_cost_without_bind(self):
         return self.number_of_pages * self.cost_per_page
--- calculator/views.py
+++ calculator/views.py
@@ -8,9 +8,11 @@ from calculator.models import XeroCalc
 
 def calculator(request):
     standard_xero_cost = 0.07
+    default_bind_cost = 0
     xero_cost = XeroCalc()
     xero_cost.cost_per_page = standard_xero_cost
     xero_cost.number_of_pages = 0
+    xero_cost.bind_cost = default_bind_cost
     data = {"xero_cost": xero_cost}
 
     return render(request, 'calc.html', context=data)
@@ -18,10 +20,12 @@ def calculator(request):
 
 def calculate(request):
     print(request.POST)
-    cost_per_page = Decimal(request.POST['cost_per_page'])/100
+    cost_per_page = Decimal(request.POST.get('cost_per_page', 0))/100
     number_of_pages = int(request.POST['number_of_pages'])
+    bind_cost = Decimal(request.POST.get('bind_cost', 0))
     xero_cost = XeroCalc()
     xero_cost.cost_per_page = cost_per_page
     xero_cost.number_of_pages = number_of_pages
+    xero_cost.bind_cost = bind_cost
     data = {"xero_cost": xero_cost}
     return render(request, 'calc.html', context=data)
