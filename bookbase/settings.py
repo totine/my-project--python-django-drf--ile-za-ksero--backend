9e174e15c6417f0e72b430d283503808c34d3757
Joanna <joanna.gargas@gmail.com>
2017-10-01 11:39:34 +0200

    Change settings for heroku (debug false, allowed hosts)

--- bookbase/settings.py
+++ bookbase/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#gmjkp)c!q9m0%^&-v22or8rq#5-bszslu1qrdv6obz2%6)5v_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['ilezaksero.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['ilezaksero.herokuapp.com', '127.0.0.1', 'localhost', 'ilezaksero.pl']
 
 
 # Application definition
