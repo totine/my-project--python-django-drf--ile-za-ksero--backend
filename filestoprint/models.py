cb0e5859eddc2592d7b05e7f14e314651aacb56c
Joanna <joanna.gargas@gmail.com>
2017-12-14 23:27:10 +0100

    Import PyPDF2 in files to print model, change File model name to FileToPrint and add path in path field

--- filestoprint/models.py
+++ filestoprint/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from bookbase.settings import MEDIA_ROOT
+import PyPDF2
 
 
-class File(models.Model):
-    path = models.FilePathField()
+class FileToPrint(models.Model):
+    path = models.FilePathField(path=MEDIA_ROOT+'files-to-print')
+
